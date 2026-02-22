6719e7e693325a0c48de724858c5db62dc5b65c1
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-22 04:49:20 -0600

    "building jpa repository interface and controller class"

--- src/main/java/com/punkpad/app/controller/ProgressionController.java
+++ src/main/java/com/punkpad/app/controller/ProgressionController.java
@@ -1,9 +1,23 @@
 package com.punkpad.app.controller;
 
+import com.punkpad.app.model.ChordProgression;
+import com.punkpad.app.repository.ChordProgressionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("")
 public class ProgressionController {
-}
+    private final ChordProgressionRepository chordProgressionRepository;
+
+    public ProgressionController(ChordProgressionRepository chordProgressionRepository) {
+        this.chordProgressionRepository = chordProgressionRepository;
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<ChordProgression> getProgressionsByUserId(@PathVariable Long userId) {
+        return chordProgressionRepository.findUserId(userId);
+    }
+@PostMapping
--- src/main/java/com/punkpad/app/repository/ChordProgressionRepository.java
+++ src/main/java/com/punkpad/app/repository/ChordProgressionRepository.java
@@ -1,7 +1,12 @@
 package com.punkpad.app.repository;
 
+import com.punkpad.app.entity.ChordProgression;
+import com.punkpad.app.model.ChordProgression;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ChordProgressionRepository {
+public class ChordProgressionRepository extends JpaRepository<ChordProgression, Long> {
+
+    List<ChordProgression> findUserId(Long userId);
+
 }
