1f66212bfe09c2a309c45511c0d02cba4de23b85
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-22 04:19:37 -0600

    "added entity models for core features"

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,7 +1,21 @@
 package model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChordProgression> chordProgressions;
 }
