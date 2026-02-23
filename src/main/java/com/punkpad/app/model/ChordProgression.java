f812bfb684366d3493c5aa9c99ff0b666210a037
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 16:17:50 -0600

    "missing annotation for ChordProgression"

--- src/main/java/com/punkpad/app/model/ChordProgression.java
+++ src/main/java/com/punkpad/app/model/ChordProgression.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Table(name = "chord_progressions")
 public class ChordProgression {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
