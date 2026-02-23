278aef2f1177d93a0d50ba4e814e0dde6d4f1f5f
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 15:49:04 -0600

    "ChordProgression getters and setters need to fix error with Subgenre"

--- src/main/java/com/punkpad/app/model/ChordProgression.java
+++ src/main/java/com/punkpad/app/model/ChordProgression.java
@@ -14,61 +14,80 @@ import java.util.List;
 public class ChordProgression {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    private Long id;
 
-        private String title;
+    private String title;
 
-        private String key;
+    private String key;
 
-        private Integer tempo;
+    private Integer tempo;
 
-      @ManyToOne
-      @JoinColumn(name = "user_id" , nullable = false)
-        private User user;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-      @ManyToOne
-      @JoinColumn(name = "sub_genre_id" , nullable = false)
-        private Subgenre subGenre;
+    @ManyToOne
+    @JoinColumn(name = "sub_genre_id", nullable = false)
+    private Subgenre subGenre;
 
-      @OneToMany(mappedBy = "progression", cascade = CascadeType.ALL, orphanRemoval = true)
-        private List<Chords> chords;
+    @OneToMany(mappedBy = "progression", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Chords> chords;
 
-      private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     public Long getId() {
         return id;
     }
 
-public String getTitle() {
-    return title;
-}
+    public String getTitle() {
+        return title;
+    }
 
-public String getKey() {
-    return key;
-}
+    public String getKey() {
+        return key;
+    }
 
-public Integer getTempo() {
-    return tempo;
-}
+    public Integer getTempo() {
+        return tempo;
+    }
 
-public User getUser() {
-    return user;
-}
+    public User getUser() {
+        return user;
+    }
 
-public Subgenre getSubGenre() {
-    return subGenre;
-}
+    public Subgenre getSubGenre() {
+        return subGenre;
+    }
 
-public List<Chords> getChords() {
-    return chords;
-}
+    public List<Chords> getChords() {
+        return chords;
+    }
 
-public LocalDateTime getCreatedAt() {
-    return createdAt;
-}
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public void setKey(String key) {
+        this.key = key;
+    }
 
+    public void setTempo(Integer tempo) {
+        this.tempo = tempo;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setSubGenre(Subgenre subGenre) {
+        this.subGenre = subGenre;
+    }
+
+    public void setChords(List<Chords> chords) {
+        this.chords = chords;
     }
 }
