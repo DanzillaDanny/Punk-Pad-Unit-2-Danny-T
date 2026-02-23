d0a9ce32aeed98466230bd405eec4b5ae9fe9641
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 13:11:14 -0600

    "trying to get classes to a place to run the app"

--- src/main/java/com/punkpad/app/model/Chords.java
+++ src/main/java/com/punkpad/app/model/Chords.java
@@ -1,7 +1,18 @@
 package com.punkpad.app.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Chords {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+    private String name;
+    private int positionIndex;
+
 }
