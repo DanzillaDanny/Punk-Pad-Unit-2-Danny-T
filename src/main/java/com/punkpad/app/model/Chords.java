d11761d54e8666b051f8bbba0a8df564a7040967
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 15:15:42 -0600

    "updating chords model"

--- src/main/java/com/punkpad/app/model/Chords.java
+++ src/main/java/com/punkpad/app/model/Chords.java
@@ -9,10 +9,11 @@ public class Chords {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
+    @Column(nullable=false, length=10)
+    private String name;
 
+    @Column(name = "position_index", nullable = false)
     private int positionIndex;
-
-    progression_id BIGINT NOT NULL,
 
     @ManyToOne
     @JoinColumn(name = "progression_id", nullable = false)
