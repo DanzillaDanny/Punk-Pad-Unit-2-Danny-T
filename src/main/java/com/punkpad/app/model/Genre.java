72ee1a9007478b21245bd66fd3a67b7fc13d886a
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 15:21:42 -0600

    "refining Genre model"

--- src/main/java/com/punkpad/app/model/Genre.java
+++ src/main/java/com/punkpad/app/model/Genre.java
@@ -1,12 +1,11 @@
 package com.punkpad.app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "genres")
 public class Genre {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
