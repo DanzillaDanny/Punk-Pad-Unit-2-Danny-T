696c66a29cb583ee83929b031139f9280f6dd972
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-23 18:48:46 -0600

    "added getters and setters to chords and will start implementing the repository"

--- src/main/java/com/punkpad/app/model/Chords.java
+++ src/main/java/com/punkpad/app/model/Chords.java
@@ -3,6 +3,7 @@ package com.punkpad.app.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name="chords")
 public class Chords {
 
     @Id
@@ -19,4 +20,26 @@ public class Chords {
     @JoinColumn(name = "progression_id", nullable = false)
     private ChordProgression progression;
 
+    public Chords(){
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPositionIndex(int positionIndex) {
+        this.positionIndex = positionIndex;
+    }
+
+    public void setProgression(ChordProgression progression) {
+        this.progression = progression;
+    }
 }
