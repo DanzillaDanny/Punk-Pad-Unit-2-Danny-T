5714f5461045ae20d700193f2e8b01084a223961
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-28 22:05:44 -0600

    "imported class and dependencies into ProgressionPatternRepository"

--- src/main/java/com/punkpad/app/repository/ProgressionPatternRepository.java
+++ src/main/java/com/punkpad/app/repository/ProgressionPatternRepository.java
@@ -1,5 +1,10 @@
 package com.punkpad.app.repository;
 
+import com.punkpad.app.model.ProgressionPattern;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProgressionPatternRepository extends JpaRepository<ProgressionPattern, Long> {
 
 }
